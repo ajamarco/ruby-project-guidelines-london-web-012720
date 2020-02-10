57d4241b7ee633b6051ccca80f1a450b4382b757
alexandre jamarco <alexandre.j@gmail.com>
2020-02-10 17:41:42 +0000

    added db folder and environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
+require_all 'app'
+
 require 'bundler'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
---
+++ db/migrate/20200210171045_create_moves.rb
@@ -0,0 +1,10 @@
+class CreateMoves < ActiveRecord::Migration[5.0]
+  def change
+    create_table :moves do |t|
+      t.string :name
+      t.string :mp_damage
+      t.integer :hp_damage
+      t.integer :cost
+    end
+  end
+end
